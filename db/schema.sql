ca4fe94cb03da2a2abd20b85fd6db47f2640d266
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-08-12 23:37:33 -0600

    created database for employees

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+\c employees_db;
+
+CREATE TABLE department {
+    id SERIAL PRIMARY KEY,
+    dep_name VARCHAR(30) UNIQUE NOT NULL
+};
+
+CREATE TABLE role {
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department INTEGER
+};
+
+CREATE TABLE employee {
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER
+}
