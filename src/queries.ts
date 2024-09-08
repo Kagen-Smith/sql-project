6422c1e6cd8d612a15f756525a1b2aa2830b6108
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-09-08 00:49:19 -0600

    added deleteDepartment function

--- src/queries.ts
+++ src/queries.ts
@@ -1,2 +1,21 @@
 import { pool } from './connection';
 
+export const viewAllDepartments = async () => {
+    const results = await pool.query(`
+        SELECT 
+            id AS "ID",
+            name AS "Name"
+        FROM
+            department
+    `);
+    return results
+};
+
+export const addDepartment = async (name: string) => {
+    const result = await pool.query(`INSERT INTO department (name) VALUES ($1) RETURNING *`, [name]);
+    return result.rows[0];
+};
+
+export const deleteDepartment = async (id: number) => {
+    const result = await pool.query(`DELETE FROM department WHERE id = $1`, [id]);
+};
