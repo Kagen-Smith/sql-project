99e7e168de11b23ec40c8f97bb0321678ae0a275
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-09-08 01:14:14 -0600

    edited connection.ts

--- src/connection.ts
+++ src/connection.ts
@@ -1,20 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import pg from 'pg';
+const { Pool } = pg;
+
 const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: 'localhost',
+    port: 5432,
     database: process.env.DB_NAME,
-    port: 5432
 });
 
+
 const connectToDb = async () => {
-const connectToDb = async =() => {
     try {
         await pool.connect();
-        console.log('Connected to the database.');
+        console.log('Connected to the database');
     } catch (err) {
-        console.error('Error connecting to database:', err);
+        console.error('Error connecting to the database:', err);
         process.exit(1);
     }
 };
-export { pool, connectToDb};
+
+export { pool, connectToDb };
